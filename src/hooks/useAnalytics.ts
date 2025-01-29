58e282880dc2241e86d53dd62d29925ec86c49c1
luigenia <luiggi.santos@genialogic.com.br>
2025-01-29 10:32:40 -0300

    fix: cookie

--- src/hooks/useAnalytics.ts
+++ src/hooks/useAnalytics.ts
@@ -7,7 +7,7 @@ export const useAnalytics = () => {
   useEffect(() => {
     ReactGA.initialize(GA_MEASUREMENT_ID, {
       gaOptions: {
-        cookieDomain: "auto",
+        cookieFlags: "SameSite=None; Secure",
       },
     });
     trackPageView();
