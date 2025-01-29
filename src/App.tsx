2ba2334c569da78f7fc7efaa9ea66ded526ce70a
luigenia <luiggi.santos@genialogic.com.br>
2025-01-29 11:06:37 -0300

    refator: chang logic in google analytic implementation

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,13 @@ import "./styles/global.scss";
 const GA_MEASUREMENT_ID = import.meta.env.VITE_GTAG_ID;
 
 function App() {
-  console.log("GA_MEASUREMENT_ID", GA_MEASUREMENT_ID);
-
   useEffect(() => {
-    ReactGA.initialize(GA_MEASUREMENT_ID);
+    ReactGA.initialize([
+      {
+        trackingId: GA_MEASUREMENT_ID,
+        gaOptions: "https://react-analytics-poc.netlify.app",
+      },
+    ]);
 
     ReactGA.send({
       hitType: "pageview",
