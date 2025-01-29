ab519a80ea616c33f14bcf170f8567d0b3f64832
luigenia <luiggi.santos@genialogic.com.br>
2025-01-29 14:55:10 -0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -12,9 +12,19 @@ export default defineConfig({
   plugins: [
     react(),
     VitePluginRadar({
-      analytics: {
-        id: GA_MEASUREMENT_ID,
-      },
+      analytics: [
+        {
+          id: GA_MEASUREMENT_ID,
+          config: {
+            cookie_domain: ".netlify.app",
+            cookie_expires: 63072000, // 2 anos
+            cookie_flags: "SameSite=None; Secure",
+            send_page_view: true,
+            allow_google_signals: true,
+            allow_ad_personalization_signals: true,
+          },
+        },
+      ],
     }),
   ],
 });
